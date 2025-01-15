cf27479fcf1719f3929ca69a7ee9918379f94cc7
alfred-mk <alfredmwaik@gmail.com>
2025-01-15 16:04:49 +0300

    log the correct data key

--- handlers/application/menuhandler.go
+++ handlers/application/menuhandler.go
@@ -184,7 +184,7 @@ func (h *MenuHandlers) SetAccountFlags(ctx context.Context, sym string, input []
 	// check language status
 	code, err := store.ReadEntry(ctx, sessionId, storedb.DATA_INITIAL_LANGUAGE_CODE)
 	if err != nil {
-		logg.ErrorCtxf(ctx, "failed to read language code entry with", "key", storedb.DATA_SELECTED_LANGUAGE_CODE, "error", err)
+		logg.ErrorCtxf(ctx, "failed to read language code entry with", "key", storedb.DATA_INITIAL_LANGUAGE_CODE, "error", err)
 		return res, nil
 	}
 
